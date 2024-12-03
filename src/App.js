d7f6b528216d0a54fe78391b9021399ec970d474
JYP <tntn3332@gmail.com>
2024-12-03 14:17:19 +0900

    Feat: useState 사용

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import Calc1 from './comp/calc/study01'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <About /> 
         <Routes>
-          <Route path={"/"} element={<Home />}/>
-          <Route path={"/about"} element={<About />}/>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/about"} element={<About />} />
+          <Route path={"/cal1"} element={<Calc1 />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
+function About() {
+  return (
+    <div style={{border: '2px blue solid'}}>
+      <Link to="/">Home으로 이동</Link>
+    </div>
+  )
+}
+
 function Home() {
   return(
     <div>
       <h1>Start Home</h1>
-      <Link to="/about">About으로 이동</Link>
-    </div>
-  )
-}
-function About() {
-  return(
-    <div>
-      <h1>About Page</h1>
-      <Link to="/">Home으로 이동</Link>
+      <Link to="/about">About으로 이동</Link><br/>
+      <Link to="/cal1">Cal1로 이동하기</Link>
+
     </div>
   )
 }
